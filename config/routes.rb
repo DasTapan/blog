7b752711e6f9f037a6ecb2ea400d51d9aa0cd054
DasTapan <tapands.das@gmail.com>
2024-07-27 12:01:27 +0530

    Create a blog with form_builder

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  resources :articles
   
   get "up" => "rails/health#show", as: :rails_health_check
 end
